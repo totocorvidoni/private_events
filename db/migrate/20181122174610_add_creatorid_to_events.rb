aa69ed2028bf0adf789b516744c0ee0a2ae11b29
Toto Corvidoni <totocorvidoni@gmail.com>
2018-11-22 15:21:07 -0300

    Add Event Creator

---
+++ db/migrate/20181122174610_add_creatorid_to_events.rb
@@ -0,0 +1,5 @@
+class AddCreatoridToEvents < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :events, :creator, foreign_key: { to_table: :users }
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_22_164611) do
+ActiveRecord::Schema.define(version: 2018_11_22_174610) do
 
   create_table "events", force: :cascade do |t|
     t.date "date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "creator_id"
+    t.index ["creator_id"], name: "index_events_on_creator_id"
   end
 
   create_table "users", force: :cascade do |t|
